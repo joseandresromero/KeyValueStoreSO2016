4d1991a41c1b6973bba3dcb091fbc3a447aaa551
Jose Romero <joanrome@espol.edu.ec>
2016-06-17 23:29:32 -0500

    Avance en libreria cliente

---
+++ libreria_cliente/Comando.java
@@ -0,0 +1,26 @@
+package libreria_cliente;
+
+public class Comando {
+
+    private String metodo;
+    private String parametro1;
+    private String parametro2;
+
+    public Comando(String metodo, String parametro1, String parametro2) {
+        this.metodo = metodo;
+        this.parametro1 = parametro1;
+        this.parametro2 = parametro2;
+    }
+
+    public String getMetodo() {
+        return this.metodo;
+    }
+
+    public String getParametro1() {
+        return this.parametro1;
+    }
+
+    public String getParametro2() {
+        return this.parametro2;
+    }
+}
---
+++ libreria_cliente/LibreriaCliente.java
@@ -0,0 +1,44 @@
+import java.io.*;
+import java.net.*;
+ 
+public class LibreriaCliente {
+
+    private Socket cliente;
+    private DataInputStream entrada;
+    private DataOutputStream salida;
+
+    public LibreriaCliente(String ipServidor, int puerto) {
+        try {
+
+            cliente = new Socket(ipServidor, puerto);
+            //asignamos este numero de puerto
+            entrada = new DataInputStream(cliente.getInputStream());
+            // será lo que enviaremos al servidor       
+            salida = new DataOutputStream(cliente.getOutputStream());
+            // será lo que nos devuelva el servidor     
+
+        } catch (UnknownHostException excepcion) {
+            System.err.println("El servidor no está levantado");
+        } catch (Exception e) {
+            System.err.println("Error: " + e );
+        }
+    }
+
+    public String ejecutarComando(Comando comando) {
+        try {
+            String linea_recibida;
+            salida.writeBytes("mi comando de prueba\n");
+            linea_recibida = entrada.readLine();
+            System.out.println("SERVIDOR DICE: " + linea_recibida);
+            salida.close();
+            entrada.close();
+            cliente.close();
+        } catch (UnknownHostException excepcion) {
+            System.err.println("No encuentro el servidor en la dirección" + ipServidor);
+        } catch (IOException excepcion) {
+            System.err.println("Error de entrada/salida");
+        } catch (Exception e) {
+            System.err.println("Error: " + e );
+        }
+    }
+}
