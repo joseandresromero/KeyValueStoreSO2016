2057ca21d7ec9dd7424fd1fad22121788f7b706a
Jose Romero <joanrome@espol.edu.ec>
2016-07-06 22:00:45 -0500

    Client worker modificado para admitir varios comandos de un cliente

--- programa_servidor/src/ClientWorker.java
+++ programa_servidor/src/ClientWorker.java
@@ -27,43 +27,26 @@ public class ClientWorker implements Runnable {
 
   public void run(){
     String line;
-    //BufferedReader in = null;
-    //PrintWriter out = null;
-/*
-    ObjectInputStream in = null;
-    ObjectOutputStream out = null;
-*/
-    BufferedReader in = null;
-    PrintWriter out = null;
-
-    try{
-   /*
-        in = new ObjectInputStream(cliente.getInputStream());
-        out= new ObjectOutputStream(cliente.getOutputStream());
-*/
-
-      in = new BufferedReader(new InputStreamReader(cliente.getInputStream()));
-      out = new PrintWriter(cliente.getOutputStream(), true);
-    } catch (IOException e) {
-      System.out.println("in or out failed");
-      System.exit(-1);
-    }
+    DataInputStream is;
+    PrintStream os;
 
 //    while(true){
       try{
-        line = in.readLine();
-        System.out.println("Linea recibida del cliente: " + line);
+	is = new DataInputStream(cliente.getInputStream());
+        os = new PrintStream(cliente.getOutputStream());
+	while (true) {
+//      is = new DataInputStream(cliente.getInputStream());
+//        os = new PrintStream(cliente.getOutputStream());
+	        line = is.readLine();
 
-	String response = CommandExecuter.executeCommand(line, store);
-
-
-//Send data back to client
-        out.println(response);
-/*
-        	Comando comando = (Comando) in.readObject();
-            System.out.println("Comando recibido del cliente: " + comando.getMetodo());
-            out.writeObject(new Respuesta(true, "Exitooooo"));
-*/
+//		if (line != null) {
+	        	System.out.println("Linea recibida del cliente: " + line);
+			String response = CommandExecuter.executeCommand(line, store);
+//			String response = CommandExecuter.test(line);
+			//Send data back to client
+		        os.println(response);
+//		}
+	}
        } catch (IOException e) {
         System.out.println("Read failed");
         System.exit(-1);
